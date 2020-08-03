87dcae46b1d7b7da6bf17fd898736e39e235c401
Stephen Rushe <srushe@users.noreply.github.com>
2020-08-03 23:20:31 +0100

    Add ICE message

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-require 'omniauth-openid'
-require 'openid/store/filesystem'
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :open_id, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id', :store => OpenID::Store::Filesystem.new('/tmp')
-end
--- db/migrate/20101219192521_add_user_to_bookmarks.rb
+++
@@ -1,9 +0,0 @@
-class AddUserToBookmarks < ActiveRecord::Migration
-  def self.up
-    add_column :bookmarks, :user_id, :integer
-  end
-
-  def self.down
-    remove_column :bookmarks, :user_id
-  end
-end
